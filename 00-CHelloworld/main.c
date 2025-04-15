d19630cc2c456728c90f3fea6411b3b6e6bcaeeb
Ser Vasquez <164660453+servasquezlizana@users.noreply.github.com>
2025-04-15 19:13:07 -0300

    Update main.c

--- 00-CHelloworld/main.c
+++ 00-CHelloworld/main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
-void say_hello(void) {
-    puts("Hola desde C23");
-}
-
 int main(void) {
-    void func(void) = say_hello; // Puntero con tipo deducido (C23)
-    func();
+    printf("Hello, World!\n");
     return 0;
 }
